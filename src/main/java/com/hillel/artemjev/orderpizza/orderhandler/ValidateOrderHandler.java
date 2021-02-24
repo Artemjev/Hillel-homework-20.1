54e3d57e689535c01505584893635f98d08773b5
Mike <artemjev.mih@gmail.com>
2021-02-24 09:42:08 +0200

    homework-20-1(update 2)

--- src/main/java/com/hillel/artemjev/orderpizza/orderhandler/ValidateOrderHandler.java
+++ src/main/java/com/hillel/artemjev/orderpizza/orderhandler/ValidateOrderHandler.java
@@ -1,15 +1,15 @@
 package com.hillel.artemjev.orderpizza.orderhandler;
 
 import com.hillel.artemjev.orderpizza.entities.Order;
+import com.hillel.artemjev.orderpizza.exception.InvalidOderDataException;
 
 public class ValidateOrderHandler extends OrderHandler {
     @Override
     public void handle(Order order) {
         if (order.getId() == null || order.getFrom() == null) {
-            System.out.println("Order data is not valid: " + order);
-//            throw new InvalidOderDataException("Order data is not valid: " + order);
-            return;
+            throw new InvalidOderDataException("Order data is not valid: " + order);
         }
+        System.out.println("Order data is valid: " + order);
         next(order);
     }
 }
